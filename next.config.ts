db20596754de7802a50b82dd63f45f147a157cf5
ramjith246 <ramjithpk2003@gmail.com>
2025-02-17 22:42:39 +0530

    Initial commit 7

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
 
 export default nextConfig;
